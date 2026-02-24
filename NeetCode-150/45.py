adbb53d95dd28de3455554b418d9158ea8debe17
MorteZ76 <mortezamojtabavi76@gmail.com>
2026-02-24 13:46:29 +0100

    45 Jump Game 2

--- NeetCode-150/45.py
+++ NeetCode-150/45.py
@@ -1,45 +1,18 @@
-
+# Not optimal solution : 
 class Solution:
-    def jump(self, nums: list[int]) -> int:
+    def jump(self, nums: List[int]) -> int:
         i = len(nums) - 1 
         dp = {}
         for j in range(1000) :
             dp[i+j] = 0
         i -= 1 
         while i >= 0 :
-            # mn = 100000000
-            # for j in range(nums[i]) :
-            #     if dp[i+j +1] + 1 < mn :
-            #         mn =  dp[i+j +1] + 1   
-            # dp [i] = mn 
-            if nums[i] != 0 :
-                dp[i] = min(dp[i+1], dp[i+nums[i]]) +1
-            else :
-                dp[i] =100000000
-            print(dp[i], i)
-            
+            mn = 100000000
+            for j in range(nums[i]) :
+                if dp[i+j +1] + 1 < mn :
+                    mn =  dp[i+j +1] + 1   
+            dp [i] = mn 
             i -= 1
 
         return dp[0]
-        
-s = Solution() 
-print(s.jump([2,3,1,1,4]))
-
-# Not optimal solution : 
-# class Solution:
-#     def jump(self, nums: List[int]) -> int:
-#         i = len(nums) - 1 
-#         dp = {}
-#         for j in range(1000) :
-#             dp[i+j] = 0
-#         i -= 1 
-#         while i >= 0 :
-#             mn = 100000000
-#             for j in range(nums[i]) :
-#                 if dp[i+j +1] + 1 < mn :
-#                     mn =  dp[i+j +1] + 1   
-#             dp [i] = mn 
-#             i -= 1
-
-#         return dp[0]
         
